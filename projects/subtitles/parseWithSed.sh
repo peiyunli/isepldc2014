5431040f2320e06e2a72f4a1fb992a47a8a4124f
unknown <aimery.thomas@outlook.com>
2014-05-23 17:54:28 +0200

    parse corrige

--- projects/subtitles/parseWithSed.sh
+++ projects/subtitles/parseWithSed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Ce srcipt retourne une valeur de la reponse json
+#Ce script retourne une valeur de la reponse json
 a='{"found":"true","downloadLink":"http:\/\/www.addic7ed.com\/updated\/1\/86593\/2","referer":"http:\/\/www.addic7ed.com\/serie\/Family_Guy\/12\/16\/Herpe the Love Sore","subName":"Family.Guy.S12E16.720p.HDTV.x264-REMARKABLE.srt","infoSub":{"quality":"REMARKABLE","link":"\/updated\/1\/86593\/2","similarity":50,"referer":"http:\/\/www.addic7ed.com\/serie\/Family_Guy\/12\/16\/Herpe the Love Sore","comment":"","similarityComment":0},"linkRef":"http:\/\/www.addic7ed.com\/updated\/1\/86593\/2?referer=http:\/\/www.addic7ed.com\/serie\/Family_Guy\/12\/16\/Herpe the Love Sore"}'
 #echo "$a"
 #found=`echo $a | sed -e 's/^.*"downloadLink"[ ]*:[ ]*"//' -e 's/".*//'`
