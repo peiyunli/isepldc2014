951503d2a2b326f495eac735e4eea73f386dfa7b
unknown <aimery.thomas@outlook.com>
2014-05-23 12:13:35 +0200

    parse terminé

--- projects/subtitles/parseWithSed.sh
+++ projects/subtitles/parseWithSed.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+#Ce srcipt retourne une valeur de la reponse json
 a='{"found":"true","downloadLink":"http:\/\/www.addic7ed.com\/updated\/1\/86593\/2","referer":"http:\/\/www.addic7ed.com\/serie\/Family_Guy\/12\/16\/Herpe the Love Sore","subName":"Family.Guy.S12E16.720p.HDTV.x264-REMARKABLE.srt","infoSub":{"quality":"REMARKABLE","link":"\/updated\/1\/86593\/2","similarity":50,"referer":"http:\/\/www.addic7ed.com\/serie\/Family_Guy\/12\/16\/Herpe the Love Sore","comment":"","similarityComment":0},"linkRef":"http:\/\/www.addic7ed.com\/updated\/1\/86593\/2?referer=http:\/\/www.addic7ed.com\/serie\/Family_Guy\/12\/16\/Herpe the Love Sore"}'
 #echo "$a"
 #found=`echo $a | sed -e 's/^.*"downloadLink"[ ]*:[ ]*"//' -e 's/".*//'`
 #echo $found
-read -p "test? " valueWanted
+read -p "Quelle valeur? " valueWanted
 #echo $valueWanted
 findArgument(){
-	#echo $a
-	echo $valueWanted
-	value=`echo $a | sed -e 's/^.*"downloadLink"[ ]*:[ ]*"//' -e 's/".*//'`
+	#echo $valueWanted
+	value= echo $a | sed -e "s/^.*\"$valueWanted\"[ ]*:[ ]*\"//" -e 's/".*//'
 	echo $value
 }
 
