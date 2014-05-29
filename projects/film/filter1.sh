18d590e859a5e8867f66c6fd5a1cdfd9cfd1f175
neirdahcier <hadrien.vercier@gmail.com>
2014-05-29 19:58:37 +0200

    add arguments and it does not delete the originals files

--- projects/film/filter1.sh
+++ projects/film/filter1.sh
@@ -9,19 +9,37 @@ FILE_RATING="ratings.list";
 FILE_DATE_TEMP="release-dates-temp.list";
 FILE_RATING_TEMP="ratings-temp.list";
 
+FILE_DATE_PARSED="release-dates-parsed.list";
+FILE_RATING_PARSED="ratings-parsed.list";
+
+YEAR="2014";
+COUNTRY="France";
+
+
+#argument for country and year
+if [ $# -eq 2 ]
+  then
+    YEAR=$1;
+    COUNTRY=$2;
+fi
+
+if [ $# -eq 1 ]
+  then
+    YEAR=$1;
+fi
 
 
 #Get films from 2014 and erase the 2 first column
-sed '/^CRC/,/^MOVIE RATINGS/d' $FILE_PATH$FILE_RATING | sed '/^--/,/^For further/d' | sed -n '/\(2014\)/p' | sed 's/\s\s*/ /g' | cut -d' ' -f4- >$FILE_PATH$FILE_RATING_TEMP;
+sed '/^CRC/,/^MOVIE RATINGS/d' $FILE_PATH$FILE_RATING | sed '/^--/,/^For further/d' | sed -n "/\($YEAR\)/p" | sed 's/\s\s*/ /g' | cut -d' ' -f4- >$FILE_PATH$FILE_RATING_TEMP;
 
 
 #Get the new film from 2014
-sed '/^CRC/,/^====/d' $FILE_PATH$FILE_DATE | sed -n '/France/p' | sed -n '/(2014)/p' > $FILE_PATH$FILE_DATE_TEMP;
+sed '/^CRC/,/^====/d' $FILE_PATH$FILE_DATE | sed -n "/$COUNTRY/p" | sed -n "/($YEAR)/p" > $FILE_PATH$FILE_DATE_TEMP;
 
 
 #we paste the content of the file temp in the actual file and remove the temp file
-cat $FILE_PATH$FILE_DATE_TEMP > $FILE_PATH$FILE_DATE;
-cat $FILE_PATH$FILE_RATING_TEMP > $FILE_PATH$FILE_RATING;
+cat $FILE_PATH$FILE_DATE_TEMP > $FILE_PATH$FILE_DATE_PARSED;
+cat $FILE_PATH$FILE_RATING_TEMP > $FILE_PATH$FILE_RATING_PARSED;
 
 
 #In order to remove all the series, we delete every lines that containts {words} and the line before this pattern.
@@ -30,24 +48,24 @@ cat $FILE_PATH$FILE_RATING_TEMP > $FILE_PATH$FILE_RATING;
 #the pattern space which contains the current line, the buffer which contains the previous line.
 #The other sed is to remove the empty lines created by the first sed
 
-sed -n '/{.*}/{s/.*//;x;d;};x;p;${x;p;}' $FILE_PATH$FILE_DATE | sed '/^$/d' > $FILE_PATH$FILE_DATE_TEMP;
-sed -n '/{.*}/{s/.*//;x;d;};x;p;${x;p;}' $FILE_PATH$FILE_RATING | sed '/^$/d' > $FILE_PATH$FILE_RATING_TEMP;
+sed -n '/{.*}/{s/.*//;x;d;};x;p;${x;p;}' $FILE_PATH$FILE_DATE_PARSED | sed '/^$/d' > $FILE_PATH$FILE_DATE_TEMP;
+sed -n '/{.*}/{s/.*//;x;d;};x;p;${x;p;}' $FILE_PATH$FILE_RATING_PARSED | sed '/^$/d' > $FILE_PATH$FILE_RATING_TEMP;
 
 #we paste the content of the file temp in the actual file and remove the temp file
-cat $FILE_PATH$FILE_DATE_TEMP > $FILE_PATH$FILE_DATE;
-cat $FILE_PATH$FILE_RATING_TEMP > $FILE_PATH$FILE_RATING;
+cat $FILE_PATH$FILE_DATE_TEMP > $FILE_PATH$FILE_DATE_PARSED;
+cat $FILE_PATH$FILE_RATING_TEMP > $FILE_PATH$FILE_RATING_PARSED;
 
 
 #We remove the line with (TV) (VG) (V) in the rating file
-sed '/(VG)/{N;d;}' $FILE_PATH$FILE_RATING | sed '/(TV)/{N;d;}'| sed '/(V)/{N;d;}'> $FILE_PATH$FILE_RATING_TEMP;
+sed '/(VG)/{N;d;}' $FILE_PATH$FILE_RATING_PARSED | sed '/(TV)/{N;d;}'| sed '/(V)/{N;d;}'> $FILE_PATH$FILE_RATING_TEMP;
 
 
 #we remove the line with certain word in the release date
-sed '/Festival)/{N;d;}' $FILE_PATH$FILE_DATE | sed '/premiere)/{N;d;}'| sed '/(TV)/{N;d;}' | sed '/(internet)/{N;d;}' | sed '/(Cannes/{N;d;}' | sed '/(limited)/{N;d;}' | sed '/(V)/{N;d;}'| sed '/(Cinema du Reel)/{N;d;}'| sed '/France:2014/{N;d;}'> $FILE_PATH$FILE_DATE_TEMP;
+sed '/Festival)/{N;d;}' $FILE_PATH$FILE_DATE_PARSED | sed '/premiere)/{N;d;}'| sed '/(TV)/{N;d;}' | sed '/(internet)/{N;d;}' | sed '/(Cannes/{N;d;}' | sed '/(limited)/{N;d;}' | sed '/(V)/{N;d;}'| sed '/(Cinema du Reel)/{N;d;}'| sed "/$COUNTRY:$YEAR/{N;d;}"> $FILE_PATH$FILE_DATE_TEMP;
 
 #we paste the content of the file temp in the actual file and remove the temp file
-cat $FILE_PATH$FILE_DATE_TEMP > $FILE_PATH$FILE_DATE;
-cat $FILE_PATH$FILE_RATING_TEMP > $FILE_PATH$FILE_RATING;
+cat $FILE_PATH$FILE_DATE_TEMP > $FILE_PATH$FILE_DATE_PARSED;
+cat $FILE_PATH$FILE_RATING_TEMP > $FILE_PATH$FILE_RATING_PARSED;
 
 #remove tempory files
 rm $FILE_PATH$FILE_DATE_TEMP;
