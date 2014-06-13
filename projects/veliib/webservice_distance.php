ccd9aba6c03ee13f8a0345840bd0f2b78ff5cef5
Dan <scemamadan@gmail.com>
2014-06-13 02:06:01 +0200

    add final project velib

---
+++ projects/veliib/webservice_distance.php
@@ -0,0 +1,17 @@
+<?php
+function get_distance_m($lat1, $lng1, $lat2, $lng2) {
+  $earth_radius = 6378137;   // Terre = sphère de 6378km de rayon
+  $rlo1 = deg2rad($lng1);
+  $rla1 = deg2rad($lat1);
+  $rlo2 = deg2rad($lng2);
+  $rla2 = deg2rad($lat2);
+  $dlo = ($rlo2 - $rlo1) / 2;
+  $dla = ($rla2 - $rla1) / 2;
+  $a = (sin($dla) * sin($dla)) + cos($rla1) * cos($rla2) * (sin($dlo) * sin($dlo
+));
+  $d = 2 * atan2(sqrt($a), sqrt(1 - $a));
+  return ($earth_radius * $d);
+}
+
+echo (round(get_distance_m($_GET['lat1'], $_GET['long1'], $_GET['lat2'], $_GET['long2']) / 1000, 3));
+?>
